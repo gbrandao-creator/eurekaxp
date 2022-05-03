085c932f746a524bbd7e8929b28366a2e66b6290
Gabriel V. Brandao <jplota9@gmail.com>
2022-05-03 18:01:02 -0300

    segundo commit, consulta-cep adicionado

---
+++ consulta-cep/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>Qual CEP você deseja pesquisar?</p>
+        <input/>
+        <button>CONSULTAR</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
